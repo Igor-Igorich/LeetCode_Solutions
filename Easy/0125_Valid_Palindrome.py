a8ef7ce6a5b31afedee052e69fc995d141f7f7d0
Igor-Igorich <igorevichPru@yandex.ru>
2026-07-09 23:06:43 +0300

    Added anover solution to task - 125 Valid Palindrome (Easy)

---
+++ Easy/0125_Valid_Palindrome.py
@@ -0,0 +1,23 @@
+
+def is_palindrome(s: str) -> bool:
+    left = 0
+    right = len(s) - 1
+    s = s.lower()
+    while left < right:
+        while not(s[left].isalnum()) and left < right:
+            left += 1
+        while not(s[right].isalnum()) and left < right:
+            right -= 1
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+def pipeline_check(func: function) -> str:
+    s_1 = "A man, a plan, a canal: Panama"
+    s_2 = "race a car"
+    s_3 = " "
+    return f'1) -> {func(s_1)}\n2) -> {func(s_2)}\n3) -> {func(s_3)}'
+
+print(pipeline_check(is_palindrome))
