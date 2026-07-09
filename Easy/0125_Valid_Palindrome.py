b6501fc4c93c3d6781a832fbf9cd4876cde445ca
Igor-Igorich <igorevichPru@yandex.ru>
2026-07-09 23:11:53 +0300

    Added anover solution to task - 125 Valid Palindrome (Easy)

--- Easy/0125_Valid_Palindrome.py
+++ Easy/0125_Valid_Palindrome.py
@@ -1,4 +1,6 @@
 
+import string
+
 def is_palindrome(s: str) -> bool:
     left = 0
     right = len(s) - 1
@@ -14,10 +16,29 @@ def is_palindrome(s: str) -> bool:
         right -= 1
     return True
 
+def primitive_is_palindrome(s: str) -> bool:
+    transform = str.maketrans("", "", string.punctuation)
+    s = "".join(s.translate(transform).lower().split())
+
+    return s == s[::-1]
+
+def bad_is_palindrome(s: str) -> bool:
+    s = ''.join(c.lower() for c in s if c.isalnum())
+    left = 0 
+    right = len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
 def pipeline_check(func: function) -> str:
     s_1 = "A man, a plan, a canal: Panama"
     s_2 = "race a car"
     s_3 = " "
     return f'1) -> {func(s_1)}\n2) -> {func(s_2)}\n3) -> {func(s_3)}'
 
-print(pipeline_check(is_palindrome))
+print(pipeline_check(primitive_is_palindrome))
